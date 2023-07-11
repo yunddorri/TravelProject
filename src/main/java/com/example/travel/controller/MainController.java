f7f7d13d9125b7d8bc7a3a1a9676ad445caf4627
yunddorri <ys.2871@daum.net>
2023-07-11 23:10:53 +0900

    Fix: "회원가입&로그인_3" Spring Security를 활용한 회원가입 기능 및 로그인 기능입니다. 전에 빠졌던 파일들 추가하였습니다.

---
+++ src/main/java/com/example/travel/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.travel.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping(value="/")
+    public String main() {
+        return "main";
+    }
+}
